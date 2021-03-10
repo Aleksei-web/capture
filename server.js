4353586ed7c3d57da6091d24252b04de066c6e15
Alexei-web <maksimushkin92@gmail.com>
2021-03-10 11:26:29 +0300

    delte favicon

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 import express from 'express';
-import favicon from 'express-favicon';
 import path from 'path';
 const port = process.env.PORT || 8080
 const __dirname = path.resolve();
 
 const app = express();
 app.use(express.static(__dirname));
-app.use(favicon(__dirname + '/build/favicon.png'));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
